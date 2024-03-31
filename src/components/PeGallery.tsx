5393c6310317b5e0157098ab009fe6663a21e811
hackername1 <62359997+hackername1@users.noreply.github.com>
2024-03-31 17:56:54 +0300

    Gallery logic and styling added.

--- src/components/PeGallery.tsx
+++ src/components/PeGallery.tsx
@@ -1,13 +1,36 @@
-import "../styles/pe-link.css";
+import React from "react";
+import "../styles/pe-gallery.css";
 
 interface PeGalleryProps {
+	columns?: 2 | 3;
 	children?: React.ReactNode;
 }
 
 const PeGallery = (props: PeGalleryProps) => {
+	let columnsClassName = "";
+	switch (props.columns) {
+		default:
+		case 2:
+			columnsClassName = "pe-gallery-2";
+			break;
+		case 3:
+			columnsClassName = "pe-gallery-3";
+			break;
+	}
+
+	const wrapChildrenWithDiv = (children: React.ReactNode) => {
+		return React.Children.map(children, (child, index) => (
+			<div key={index} className="pe-gallery-item">
+				{child}
+			</div>
+		));
+	};
+
+	const wrappedChildren = props.children ? wrapChildrenWithDiv(props.children) : null;
+
 	return (
-		<div className="pe-gallery">
-			{props.children}
+		<div className={"pe-gallery " + columnsClassName}>
+			{wrappedChildren}
 		</div>
 	)
 };
