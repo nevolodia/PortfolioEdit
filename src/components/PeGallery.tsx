c42afd71a4db59fd93ad02bf255216ea112bfa86
hackername1 <62359997+hackername1@users.noreply.github.com>
2024-03-31 23:16:46 +0300

    Advanced last row no bottom margin logic added.

--- src/components/PeGallery.tsx
+++ src/components/PeGallery.tsx
@@ -18,15 +18,53 @@ const PeGallery = (props: PeGalleryProps) => {
 			break;
 	}
 
-	const wrapChildrenWithDiv = (children: React.ReactNode) => {
-		return React.Children.map(children, (child, index) => (
-			<div key={index} className="pe-gallery-item">
-				{child}
+	let childrenCount = React.Children.count(props.children);
+	const wrapChildrenWithDiv = (children: React.ReactNode, index: number) => {
+		let noMargin = false;
+		let noMarginMobile = false;
+
+		switch (props.columns) {
+			default:
+			case 2:
+				if ((index === childrenCount - 1) && (childrenCount >= 3)) {
+					noMargin = true;
+					noMarginMobile = true;
+				} else if ((index === childrenCount - 2) && (childrenCount >= 4)) {
+					noMargin = true
+				}
+				break;
+			case 3:
+				if (index === childrenCount - 1) {
+					noMargin = true;
+					noMarginMobile = true;
+				} else if ((index === childrenCount - 2) && (childrenCount >= 5)) {
+					noMargin = true;
+				} else if ((index === childrenCount - 3) && (childrenCount >= 6)) {
+					noMargin = true;
+				}
+
+				if ((index === childrenCount - 2) && (childrenCount >= 4)) {
+					noMarginMobile = true;
+				}
+				break;
+		}
+
+		let noMarginClassName = noMargin ? "pe-gallery-item-no-margin" : "";
+		let noMarginMobileClassName = noMarginMobile ? "pe-gallery-item-no-margin-mobile" : "";
+
+		return (
+			<div className={"pe-gallery-item " + noMarginClassName + " " + noMarginMobileClassName}>
+				{children}
 			</div>
-		));
+		);
 	};
 
-	const wrappedChildren = props.children ? wrapChildrenWithDiv(props.children) : null;
+	let wrappedChildren: React.ReactNode[] = [];
+	if (props.children) {
+		React.Children.toArray(props.children).map((child: React.ReactNode, index: number) => {
+			wrappedChildren.push(wrapChildrenWithDiv(child, index));
+		});
+	}
 
 	return (
 		<div className={"pe-gallery " + columnsClassName}>
