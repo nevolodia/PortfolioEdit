98bc407b42f15650b4a56b4778a6d7f703f79a8b
hackername1 <62359997+hackername1@users.noreply.github.com>
2024-04-01 17:51:06 +0300

    Video component added (uses Plyr player).

---
+++ src/components/PeVideo.tsx
@@ -0,0 +1,54 @@
+import React, {useRef} from "react";
+import Plyr, {APITypes} from "plyr-react";
+import "plyr-react/plyr.css"
+
+interface PeVideoProps {
+	src: string;
+	provider?: Plyr.Provider;
+}
+
+const PeVideo = (props: PeVideoProps) => {
+	let controls = window.innerWidth >= 576 ?
+		[ "play", "progress", "current-time", "mute", "volume", "settings", "fullscreen" ] :
+		[ "play", "progress", "current-time", "settings", "fullscreen", ];
+
+	let plyrProvider = props.provider ? props.provider : "html5";
+
+	const ref = useRef<APITypes>(null);
+	const plyrVideo =
+		props.src ? (
+			<Plyr
+				ref={ref}
+				source={{
+					type: "video",
+					sources: [
+						{
+							provider: plyrProvider,
+							src: props.src,
+						},
+					]
+				}}
+				options={
+					{
+						controls: controls,
+						settings: [
+							"quality",
+							"speed",
+						],
+						speed: {
+							selected: 1,
+							options: [0.5, 0.75, 1, 1.25, 1.5, 1.75, 2],
+						},
+					}
+				}
+			/>
+		) : null;
+
+	return (
+		<div>
+			{plyrVideo}
+		</div>
+	);
+}
+
+export default PeVideo;
