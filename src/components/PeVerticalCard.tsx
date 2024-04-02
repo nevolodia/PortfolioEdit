b2268a5ffbbd3dfc2d7af2affbb78f315d41b6cf
hackername1 <62359997+hackername1@users.noreply.github.com>
2024-04-02 17:16:21 +0300

    Typo renames.

--- src/components/PeVerticalCard.tsx
+++ src/components/PeVerticalCard.tsx
@@ -10,26 +10,26 @@ interface PeVerticalCardProps {
 
 const PeVerticalCard = (props: PeVerticalCardProps) => {
 	return (
-		<div className="pe-card pe-card-vertical">
+		<div className="pe-vertical-card">
 
 			{props.imageSrc && (
-				<img className="pe-card-image"
+				<img className="pe-vertical-card-image"
 				     src={props.imageSrc}
 				     alt={props.title}
 				/>
 			)}
 
-			<div className="pe-card-content">
-				<div className="pe-card-content-top">
+			<div className="pe-vertical-card-content">
+				<div className="pe-vertical-card-content-top">
 					<PeTitle size={2} weight="normal" bottomMargin="no-margin">
 						{props.title}
 					</PeTitle>
-					<h6 className="pe-card-subtitle">
+					<h6 className="pe-vertical-card-subtitle">
 						{props.tag}
 					</h6>
 				</div>
 				{props.description && (
-					<p className="pe-card-description">
+					<p className="pe-vertical-card-description">
 						{props.description}
 					</p>
 				)}
