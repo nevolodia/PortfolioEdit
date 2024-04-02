b7adeb239b9681dceb87e79eff132a356511d416
hackername1 <62359997+hackername1@users.noreply.github.com>
2024-04-02 12:55:44 +0300

    Link no styling property added.

--- src/components/PeLink.tsx
+++ src/components/PeLink.tsx
@@ -3,19 +3,24 @@ import "../styles/pe-link.css";
 interface PeLinkProps {
 	href: string;
 	horizontalSize?: "wide" | "normal";
+	noStyling?: boolean;
 	children?: React.ReactNode;
 }
 
 const PeLink = (props: PeLinkProps) => {
+	let linkClass = props.noStyling ? "" : "pe-link-styling ";
+
 	let horizontalSizeClass = "";
-	switch (props.horizontalSize) {
-		case "wide":
-			horizontalSizeClass = "pe-link-horizontal-size-wide";
-			break;
+	if (!props.noStyling) {
+		switch (props.horizontalSize) {
+			case "wide":
+				horizontalSizeClass = "pe-link-horizontal-size-wide";
+				break;
+		}
 	}
 
 	return (
-		<a className={"pe-link " + horizontalSizeClass}
+		<a className={"pe-link " + linkClass + horizontalSizeClass}
 		   href={props.href}
 		>
 			{props.children}
