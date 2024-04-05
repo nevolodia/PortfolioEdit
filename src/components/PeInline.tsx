1bb94e97c215ed5287125cfbeef0f40305d7a249
hackername1 <62359997+hackername1@users.noreply.github.com>
2024-04-05 21:10:56 +0300

    Same child widths in Pe Inline property added.

--- src/components/PeInline.tsx
+++ src/components/PeInline.tsx
@@ -2,12 +2,15 @@ import "../styles/pe-inline.css";
 import React from "react";
 
 interface PeInlineProps {
+	sameWidth?: boolean;
 	children?: React.ReactNode;
 }
 
 const PeInline = (props: PeInlineProps) => {
+	let sameWidthClassNames = props.sameWidth ? "pe-inline-same-width" : "";
+
 	return (
-		<div className="pe-inline">
+		<div className={"pe-inline " + sameWidthClassNames}>
 			{props.children}
 		</div>
 	)
