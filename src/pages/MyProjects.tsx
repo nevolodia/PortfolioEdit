f4f5cefca8ec41a69f562671fba17da17073a21b
hackername1 <62359997+hackername1@users.noreply.github.com>
2024-04-05 21:12:45 +0300

    Project cards added.

--- src/pages/MyProjects.tsx
+++ src/pages/MyProjects.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import PeTitle from "../components/PeTitle";
-import PeImage from "../components/PeImage";
+import PeInline from "../components/PeInline";
 import PeVerticalCard from "../components/PeVerticalCard";
+import PeHorizontalCard from "../components/PeHorizontalCard";
 import hackermangif from "../assets/images/hackerman.gif";
+import binatec from "../assets/images/binatec01.jpg";
 
 const MyProjectsPage: React.FC = () => {
 	return (
@@ -12,16 +14,26 @@ const MyProjectsPage: React.FC = () => {
 				More projects to come!
 			</PeTitle>
 
-			<PeVerticalCard title="PortfolioEdit"
-			                tag="In development"
-			                description="This very project you see. Made with React and TypeScript.
-			                Here is used as my portfolio, but can and wellcome to be used as portfolio for anyone!"
-			                linkHref="https://github.com/hackername1/PortfolioEdit"
-			/>
+			<PeInline sameWidth >
 
-			<PeImage src={hackermangif}
-			         alt="meme gif"
-			/>
+				<PeHorizontalCard title="PortfolioEdit"
+				                  tag="In development"
+				                  description="This very project you see. Made with React and TypeScript.
+			                  Here is used as my portfolio, but can and wellcome to be used as portfolio for anyone!"
+				                  imageSrc={hackermangif}
+				                  linkHref="https://github.com/hackername1/PortfolioEdit"
+				                  maxWidth="400px"
+				/>
+				<PeHorizontalCard title="Binatec"
+				                  tag="In development"
+				                  description="Baltic shop with revolutionary household chemistry products which are
+				                  based on biological components. Website made with WordPress and WooCommerce."
+				                  imageSrc={binatec}
+				                  linkHref="https://binatec.eu/"
+				                  maxWidth="400px"
+				/>
+
+			</PeInline>
 		</>
 	);
 }
